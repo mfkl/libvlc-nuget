40a2d1fa561eab7cb807bb04d75caf695d0efe9c
Martin Finkel <martin@videolabs.io>
2018-07-30 13:58:57 +0200

    Fix Mac packaging
    Use a .framework for now

--- package-nuget-mac.sh
+++ package-nuget-mac.sh
@@ -1,28 +1,3 @@
 #! /bin/bash
-
-version=$1
-downloadUrl="http://download.videolan.org/pub/videolan/vlc/$version/macosx/vlc-$version.dmg"
-
-packageName="VideoLAN.LibVLC.Mac"
-
-echo "downloading binaries..." $downloadUrl
-curl -Lsfo mac.dmg $downloadUrl
-
-echo "downloading NuGet..."
-curl -Lsfo nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
-echo "unzipping vlc..."
-7z x mac.dmg -o./mac 
-7z x mac/4.hfs
-
-echo "copying dlls, libs and headers files..."
-mkdir -p build/osx-x64/
-cp -R mac/VLC\ media\ player/VLC.app/Contents/MacOS/{include,lib,plugins,share} build/osx-x64/
-
-echo "cleaning up..."
-rm -rf ./mac ./VLC\ media\ player ./mac.dmg
-
-echo "packaging"
-
-mono nuget.exe pack "$packageName".nuspec -Version "$version"
-
-echo "done"
+#./compile
+nuget pack "VideoLAN.LibVLC.Mac.nuspec" -Version "$version"
