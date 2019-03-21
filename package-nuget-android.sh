9b14276a532735f92b009e258c7816ec0440b38e
Martin Finkel <martin@videolabs.io>
2019-03-21 15:54:01 +0100

    Android Packaging changes for libvlc 3.1
    - include libc++_shared by default
    - workaround 7zip bug

--- package-nuget-android.sh
+++ package-nuget-android.sh
@@ -1,48 +1,46 @@
 #! /bin/bash
 
-version=$1
+version="3.1.2" # vlc-android tag
 echo "packaging version $version"
 
-downloadUrlArm7="https://download.videolan.org/pub/videolan/vlc-android/$version/VLC-Android-$version-ARMv7.apk"
-downloadUrlArm8="https://download.videolan.org/pub/videolan/vlc-android/$version/VLC-Android-$version-ARMv8.apk"
+downloadUrlArm7="https://download.videolan.org/pub/videolan/vlc-android/$version/VLC-Android-$version-armeabi-v7a.apk"
+downloadUrlArm8="https://download.videolan.org/pub/videolan/vlc-android/$version/VLC-Android-$version-arm64-v8a.apk"
 downloadUrlx86="https://download.videolan.org/pub/videolan/vlc-android/$version/VLC-Android-$version-x86.apk"
 downloadUrlx86_64="https://download.videolan.org/pub/videolan/vlc-android/$version/VLC-Android-$version-x86_64.apk"
+
+sourceUrl="https://github.com/videolan/vlc-android/archive/$version.zip"
 
 packageName="VideoLAN.LibVLC.Android"
 echo "package name $packageName"
 
-curl -Lsfo arm7.7z $downloadUrlArm7 --progress-bar --verbose
-curl -Lsfo arm8.7z $downloadUrlArm8 --progress-bar --verbose
-curl -Lsfo x86.7z $downloadUrlx86 --progress-bar --verbose
-curl -Lsfo x86_64.7z $downloadUrlx86_64 --progress-bar --verbose
+# curl -Lsfo VLC-Android-$version-armeabi-v7a.apk $downloadUrlArm7 --progress-bar --verbose
+# curl -Lsfo VLC-Android-$version-arm64-v8a.apk $downloadUrlArm8 --progress-bar --verbose
+# curl -Lsfo VLC-Android-$version-x86.apk $downloadUrlx86 --progress-bar --verbose
+# curl -Lsfo VLC-Android-$version-x86_64.apk $downloadUrlx86_64 --progress-bar --verbose
 
 echo "downloading NuGet..."
-curl -Lsfo nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+# curl -Lsfo nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
 
-echo "unzipping vlc..."
-7z x arm7.7z -o./arm7
-7z x arm8.7z -o./arm8
-7z x x86.7z -o./x86
-7z x x86_64.7z -o./x86_64
+echo "unzipping libvlc.so and libc++_shared.so..."
+7z e VLC-Android-$version-armeabi-v7a.apk lib/armeabi-v7a/libvlc.so lib/armeabi-v7a/libc++_shared.so -obuild/android-armv7 -y
+7z e VLC-Android-$version-arm64-v8a.apk lib/arm64-v8a/libvlc.so lib/arm64-v8a/libc++_shared.so -obuild/android-armv8 -y
+7z e VLC-Android-$version-x86.apk lib/x86/libvlc.so lib/x86/libc++_shared.so -obuild/android-x86 -y
+7z e VLC-Android-$version-x86_64.apk lib/x86_64/libvlc.so lib/x86_64/libc++_shared.so -obuild/android-x86_64 -y
 
-echo "copying x86 dlls, libs and headers files..."
+# workaround a 7zip/nuget bug (datetime 0)
+touch build/android-armv7/libvlc.so
+touch build/android-armv7/libc++_shared.so
+touch build/android-armv8/libvlc.so
+touch build/android-armv8/libc++_shared.so
+touch build/android-x86/libvlc.so
+touch build/android-x86/libc++_shared.so
+touch build/android-x86_64/libvlc.so
+touch build/android-x86_64/libc++_shared.so
 
-mkdir -p build/android-armv7/
-cp ./arm7/lib/armeabi-v7a/libvlcjni.so build/android-armv7/
-mv build/android-armv7/libvlcjni.so build/android-armv7/libvlc.so
+# curl -Lsfo sources.zip $sourceUrl --progress-bar --verbose
 
-mkdir -p build/android-armv8/
-cp ./arm8/lib/arm64-v8a/libvlcjni.so build/android-armv8/
-mv build/android-armv8/libvlcjni.so build/android-armv8/libvlc.so
+# 7z e sources.zip vlc-android-$version/libvlc/src/org/videolan/libvlc/AWindow.java -obuild/toBuild
+# 7z e sources.zip vlc-android-$version/libvlc/src/org/videolan/libvlc/IVLCVout.java -obuild/toBuild
+# 7z e sources.zip vlc-android-$version/libvlc/src/org/videolan/libvlc/util/AndroidUtil.java -obuild/toBuild
 
-mkdir -p build/android-x86/
-cp ./x86/lib/x86/libvlcjni.so build/android-x86/
-mv build/android-x86/libvlcjni.so build/android-x86/libvlc.so
-
-mkdir -p build/android-x86_64/
-cp ./x86_64/lib/x86_64/libvlcjni.so build/android-x86_64/
-mv build/android-x86_64/libvlcjni.so build/android-x86_64/libvlc.so
-
-echo "done"
-
-mono nuget.exe pack "$packageName".nuspec -Version "$version"
+#mono nuget.exe pack "$packageName".nuspec -Version "$version"
