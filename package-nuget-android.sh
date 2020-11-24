6825cbcbf8b83e4e917f016bd74033e545dc77d1
Martin Finkel <me@martinfinkel.com>
2020-11-24 14:47:52 +0700

    update script to download from bintray

--- package-nuget-android.sh
+++ package-nuget-android.sh
@@ -3,39 +3,48 @@
 version=$1
 echo "packaging version $version"
 
-downloadUrlArm7="https://download.videolan.org/pub/videolan/vlc-android/$version/VLC-Android-$version-armeabi-v7a.apk"
-downloadUrlArm8="https://download.videolan.org/pub/videolan/vlc-android/$version/VLC-Android-$version-arm64-v8a.apk"
-downloadUrlx86="https://download.videolan.org/pub/videolan/vlc-android/$version/VLC-Android-$version-x86.apk"
-downloadUrlx86_64="https://download.videolan.org/pub/videolan/vlc-android/$version/VLC-Android-$version-x86_64.apk"
+downloadUrlBinary="https://bintray.com/videolan/Android/download_file?file_path=org%2Fvideolan%2Fandroid%2Flibvlc-all%2F3.3.8%2Flibvlc-all-3.3.8.aar"
+# downloadUrlArm8="https://download.videolan.org/pub/videolan/vlc-android/$version/VLC-Android-$version-arm64-v8a.apk"
+# downloadUrlx86="https://download.videolan.org/pub/videolan/vlc-android/$version/VLC-Android-$version-x86.apk"
+# downloadUrlx86_64="https://download.videolan.org/pub/videolan/vlc-android/$version/VLC-Android-$version-x86_64.apk"
 
-sourceUrl="https://github.com/videolan/vlc-android/archive/$version.zip"
+sourceUrl="https://bintray.com/videolan/Android/download_file?file_path=org%2Fvideolan%2Fandroid%2Flibvlc-all%2F3.3.8%2Flibvlc-all-3.3.8-sources.jar"
 
 packageName="VideoLAN.LibVLC.Android"
 echo "package name $packageName"
 
-# curl -Lsfo arm7.7z $downloadUrlArm7 --progress-bar --verbose
-# curl -Lsfo arm8.7z $downloadUrlArm8 --progress-bar --verbose
-# curl -Lsfo x86.7z $downloadUrlx86 --progress-bar --verbose
-# curl -Lsfo x86_64.7z $downloadUrlx86_64 --progress-bar --verbose
+# curl -Lsfo binary.7z $downloadUrlBinary --progress-bar --verbose
 
 echo "downloading NuGet..."
 # curl -Lsfo nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
 
 echo "unzipping libvlc.so ..."
-# 7z e arm7.7z lib/armeabi-v7a/libvlc.so -obuild/android-armv7
-# 7z e arm8.7z lib/arm64-v8a/libvlc.so -obuild/android-armv8
-# 7z e x86.7z lib/x86/libvlc.so -obuild/android-x86
-# 7z e x86_64.7z lib/x86_64/libvlc.so -obuild/android-x86_64
+7z e binary.7z jni/armeabi-v7a/libvlc.so -obuild/android-armv7 -aoa
+7z e binary.7z jni/armeabi-v7a/libc++_shared.so -obuild/android-armv7 -aoa
+
+7z e binary.7z jni/arm64-v8a/libvlc.so -obuild/android-armv8 -aoa
+7z e binary.7z jni/arm64-v8a/libc++_shared.so -obuild/android-armv8 -aoa
+
+7z e binary.7z jni/x86/libvlc.so -obuild/android-x86 -aoa
+7z e binary.7z jni/x86/libc++_shared.so -obuild/android-x86 -aoa
+
+7z e binary.7z jni/x86_64/libvlc.so -obuild/android-x86_64 -aoa
+7z e binary.7z jni/x86_64/libc++_shared.so -obuild/android-x86_64 -aoa
 
 touch build/android-armv7/libvlc.so
+touch build/android-armv7/libc++_shared.so
+
 touch build/android-armv8/libvlc.so
+touch build/android-armv8/libc++_shared.so
+
 touch build/android-x86/libvlc.so
+touch build/android-x86/libc++_shared.so
+
 touch build/android-x86_64/libvlc.so
+touch build/android-x86_64/libc++_shared.so
 
-#curl -Lsfo sources.zip $sourceUrl --progress-bar --verbose
+# curl -Lsfo sources.zip $sourceUrl --progress-bar --verbose
 
-7z e sources.zip vlc-android-$version/libvlc/src/org/videolan/libvlc/AWindow.java -obuild/toBuild
-7z e sources.zip vlc-android-$version/libvlc/src/org/videolan/libvlc/IVLCVout.java -obuild/toBuild
-7z e sources.zip vlc-android-$version/libvlc/src/org/videolan/libvlc/util/AndroidUtil.java -obuild/toBuild
-
-#mono nuget.exe pack "$packageName".nuspec -Version "$version"
+# 7z e sources.zip org/videolan/libvlc/AWindow.java -obuild/toBuild -aoa
+# 7z e sources.zip org/videolan/libvlc/interfaces/IVLCVout.java -obuild/toBuild -aoa
+# 7z e sources.zip org/videolan/libvlc/util/AndroidUtil.java -obuild/toBuild -aoa
